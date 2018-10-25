8b94b32768496d7a854ce3f246a017df259e2526
georgemmp <machadogeorge.p@gmail.com>
2018-10-25 19:08:00 -0300

    Cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const mogoose = require('mongoose')
+const cors = require('cors')
+const mongoose = require('mongoose')
 const requireDir = require('require-dir')
 const routes = require('./src/routes')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
 
